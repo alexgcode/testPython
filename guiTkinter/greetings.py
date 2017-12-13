0ac7697b18212f0668c5e76d063f3f6588046f57
Alex Garcia <alexgf2703@gmail.com>
2017-12-13 18:52:47 -0500

    window with tkinter

---
+++ guiTkinter/greetings.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import sys, os
+from PIL import ImageTk, Image
+from tkinter import Tk, Label, Button
+
+class MyFirstGUI:
+    def __init__(self, master):
+        
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button=Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+root = tk.Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
