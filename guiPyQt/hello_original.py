b7a300888df2c76cbc4a491d27ad399ba559fbc2
Alex Garcia <alexgf2703@gmail.com>
2017-12-16 23:10:41 -0500

    quitButton

--- guiPyQt/hello_original.py
+++ guiPyQt/hello_original.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton #variables globales
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
 
 class Example(QWidget):
 	def __init__(self):
@@ -9,24 +8,17 @@ class Example(QWidget):
 		self.initUI()
 
 	def initUI(self):
-		
+		btn = QPushButton('Button', self)
+		btn.resize(btn.sizeHint())
+		btn.move(50,50)
 
 		self.setGeometry(300,300,300,220)
 		self.setWindowTitle('Icon')
 		self.setWindowIcon(QIcon('icon.png')) 
-		self.home()
-
-	def home(self):
-		btn = QPushButton('Quit', self)
-		btn.clicked.connect(self.quitmethod)	#cerrar programa
-		btn.resize(btn.sizeHint())
-		btn.move(50,50)
 		self.show()
-
-	def quitmethod():
-		QCoreApplication.instance().quit
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	ex = Example()	#instanciacion de la clase(creacion de objeto) para que se muestre la ventana
+	print(ex)
 	sys.exit(app.exec_())
